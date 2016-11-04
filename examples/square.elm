d2b9899133b4dc7b00fdd374a549599a487dff6b
Pietro909 <pietro.grandi.3d@gmail.com>
2016-11-04 16:30:22 +0100

    example with squares and lines

--- examples/square.elm
+++ examples/square.elm
@@ -8,7 +8,44 @@ import Html.Attributes exposing (width, height)
 import Html exposing (div, text, p)
 import AnimationFrame
 
+type alias Vertex = { position : Vec3, color : Vec3 }
 
+
+line : V2.Vec2 -> V2.Vec2 -> Vec3 -> Drawable Vertex
+line from to color =
+  Lines
+    [( Vertex (vec3 (V2.getX from) (V2.getY from) 0) color
+    , Vertex (vec3 (V2.getX to) (V2.getY to) 0) color
+    )]
+
+
+square : V2.Vec2 -> Float -> Vec3 -> Drawable Vertex
+square position size color =
+  rectangle position size size color
+
+
+rectangle : V2.Vec2 -> Float -> Float -> Vec3 -> Drawable Vertex
+rectangle position width height color =
+  let
+    x = V2.getX position
+    y = V2.getY position
+    halfWidth = width / 2
+    halfHeight = height / 2
+  in
+    TriangleFan 
+      [ Vertex (vec3 (x + halfWidth) (y + halfHeight) 0) color
+      , Vertex (vec3 (x - halfWidth) (y + halfHeight) 0) color
+      , Vertex (vec3 (x - halfWidth) (y - halfHeight) 0) color
+      , Vertex (vec3 (x + halfWidth) (y - halfHeight) 0) color
+      ]
+
+
+mesh1 = square (V2.vec2 0.0 0.4) 0.2 (vec3 0.2 1.0 0.3)
+mesh2 = square (V2.vec2 0.2 0) 0.2 (vec3 0.2 1.0 0.3)
+mesh3 = square (V2.vec2 -0.2 0) 0.2 (vec3 0.2 1.0 0.3)
+mesh4 = line (V2.vec2 0.0 0.4) (V2.vec2 0.2 0) (vec3 1.0 0.1 0.0)
+mesh5 = line (V2.vec2 0.0 0.4) (V2.vec2 -0.2 0) (vec3 1.0 0.1 0.0)
+ 
 main : Program Never
 main =
   Html.program
@@ -19,88 +56,19 @@ main =
     }
 
 
-type alias Vertex = { position : Vec3, color : Vec3 }
-
-type alias Model =
-  { sides : Int
-  , radius : Float
-  , color : Vec3
-  , mesh : Drawable Vertex
-  }
-
-type Msg
-  = Sides Int
-  | Radius Float
-  | Color Vec3
-
-
-update : Model -> Msg -> Model
-update model msg =
-  case msg of
-    Sides sides ->
-      let
-        drawable = ngon (V2.vec2 0 0) sides model.radius model.color
-      in
-        { model | sides = sides, mesh = drawable }
-    Radius radius ->
-      let
-        drawable = ngon (V2.vec2 0 0) model.sides radius model.color
-      in
-        { model | radius = radius, mesh = drawable }
-
-
-view : Model -> Html a
-view model =
-  article []
-    [ aside []
-      [ div []
-        [ label [] [ text "Sides" ]
-        , input [ type' "number", min 3, max 16, step 1, value model.sides, onInput Sides ] []
-        ]
-      , div []
-        [ label [] [ text "Radius" ]
-        , input [ type' "number", min 0.1, max 1.0, step 0.01, value model.radius, onInput Radius ] []
-        ]
-      ]
-    , section []
-      [ WebGL.toHtml
-        [ width 400, height 400 ]
-        [ WebGL.render vertexShader fragmentShader model.mesh { } ]
-      ]
+view : Float -> Html msg
+view t =
+  WebGL.toHtml
+    [ width 400, height 400 ]
+    [ WebGL.render vertexShader fragmentShader mesh4 { }
+    , WebGL.render vertexShader fragmentShader mesh5 { }
+    , WebGL.render vertexShader fragmentShader mesh1 { }
+    , WebGL.render vertexShader fragmentShader mesh2 { }
+    , WebGL.render vertexShader fragmentShader mesh3 { }
     ]
 
 
-nextNGonPoint : Float -> Int -> List V2.Vec2 -> List V2.Vec2
-nextNGonPoint alpha current acc =
-  let
-    x = cos (alpha * (toFloat current))
-    y = sin (alpha * (toFloat current))
-    vertex = V2.vec2 x y
-    next = current - 1
-  in
-    if current == 0 then
-      vertex::acc
-    else
-      nextNGonPoint alpha next (vertex::acc)
-
-ngon : V2.Vec2 -> Int -> Float -> Vec3 -> Drawable Vertex
-ngon position sides radius color =
-  let
-    alpha : Float
-    alpha = (pi * 2.0 / (toFloat sides)) --+ (pi/4.0)
-    vertices = nextNGonPoint alpha (sides - 1) []
-    vec2ToVertex = (\v -> 
-      { position = vec3 (V2.getX v) (V2.getY v) 0, color = color }
-    )
-  in
-    List.map vec2ToVertex vertices
-      |> TriangleFan
-    
-
- 
-
-
--- SHADERS
+-- Shaders
 
 vertexShader :
   Shader
